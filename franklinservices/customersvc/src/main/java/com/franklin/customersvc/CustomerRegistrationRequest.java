15750e2e19a61226011ad68c9a7fcaf8c5fcef5a
Franklin Velasquez <iefranklin@hotmail.es>
2023-02-03 23:58:04 -0600

    add N-tiers for CustomerSVC: Controller, Service, Data models

---
+++ franklinservices/customersvc/src/main/java/com/franklin/customersvc/CustomerRegistrationRequest.java
@@ -0,0 +1,4 @@
+package com.franklin.customersvc;
+
+public record CustomerRegistrationRequest(String firstName, String lastName, String email) {
+}
---
+++ franklinservices/customersvc/src/main/java/com/franklin/customersvc/CustomerService.java
@@ -0,0 +1,17 @@
+package com.franklin.customersvc;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public record CustomerService() {
+    public void registerCustomer(CustomerRegistrationRequest request) {
+        Customer customer = Customer.builder()
+                .firstName(request.firstName())
+                .lastName(request.lastName())
+                .email(request.email())
+                .build();
+
+        //TODO: check if email is valid, check if email not taken, store customer in db.
+
+    }
+}
